a8c18191a978ba81a2161da4f2c4c4c942737eb2
GuiPastorello <guilherme.pastorello@sptech.school>
2026-09-16 19:33:17 -0300

    fix: Arruma campo de status de cadastrar torre e na tela de selecionar a torre

--- public/js/selecao_torre.js
+++ public/js/selecao_torre.js
@@ -23,12 +23,11 @@ async function carregarTorres() {
 }
 
 function ordenarTorres(lista) {
-  const prioridade = {
-    Alerta: 1,
-    Regular: 2,
-  };
-
-  return lista.sort((a, b) => prioridade[a.status] - prioridade[b.status]);
+  return [...lista].sort((a, b) => {
+    const codigoA = (a.codigo || "").toString().toLowerCase();
+    const codigoB = (b.codigo || "").toString().toLowerCase();
+    return codigoA.localeCompare(codigoB);
+  });
 }
 
 function renderizarMineradoras(grupos) {
@@ -57,13 +56,10 @@ function renderizarMineradoras(grupos) {
 }
 
 function criarCardTorre(torre) {
-  const statusClasse = torre.status.toLowerCase();
-
   return `
     <div class="torre-div">
         <div class="torre-card" data-id-torre="${torre.id_torre}">
             <img src="./imgs/institucional/LogoBanner.png" class="torre-icon" alt="Logo" />
-            <span class="torre-status ${statusClasse}">${torre.status}</span>
         </div>
         <span class="torre-codigo">${torre.codigo}</span>
     </div>
